a74e9555e431522c01455b981c804bc8bcd68205
sayyyho <323psh@naver.com>
2022-08-03 15:42:35 +0900

    [canvas] getContext

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+// ctx = context >> 붓 역할
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(0,0,100,200);
