61554b8f2ba3643d0d5ba7c208e95f315703d436
sayyyho <323psh@naver.com>
2022-08-06 14:48:47 +0900

    using canvas ctx, create man

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const canvas = document.querySelector("canvas");
 // ctx = context >> 붓 역할
 const ctx = canvas.getContext("2d");
-
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 10);
-ctx.moveTo(200,200);
-ctx.lineTo(325,100);
-ctx.lineTo(450,200);
+ctx.fillRect(200, 200, 10, 100); //arm1
+ctx.fillRect(300, 200, 10, 100); //arm2
+ctx.fillRect(225, 180 + 15, 60, 150); //body
+
+ctx.arc(255,150,30,0,Math.PI*2);
+ctx.fill()
+ctx.beginPath();
+ctx.arc(240,145,5,Math.PI,Math.PI * 2);
+ctx.arc(270,145,5,Math.PI,Math.PI * 2);
+ctx.fillStyle = "white";
 ctx.fill();
-ctx.moveTo(200,400);
-ctx.lineTo(450,400);
-ctx.stroke();  
