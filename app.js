769583d5b6b124cd184896db6ad8563dc08c8592
sayyyho <323psh@naver.com>
2022-08-06 14:04:16 +0900

    how to make line in canvas : a fundamental method

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill(); //fill or stroke
-ctx.beginPath(); // new path 이전 작업을 끝내고 새로운 특성을 도입하기 위해서.
-ctx.rect(350, 350, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill();
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50,50);
+ctx.stroke();
