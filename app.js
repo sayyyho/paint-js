e5847f9e028c59a037d82511e20e14eea8760f06
sayyyho <323psh@naver.com>
2022-08-26 00:53:25 +0900

    implement lineWidth

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width")
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 1.5;
-
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
-function onMove(location) {
+const onMove = (location) => {
     if(isPainting) {
         ctx.lineTo(location.offsetX, location.offsetY);
         ctx.stroke();
         return;
     }
-    console.log(location.offsetX, location.offsetY);
+    // console.log(location.offsetX, location.offsetY);
     ctx.moveTo(location.offsetX, location.offsetY);
 }
 
-function startPaint() {
-    isPainting = true ;
+const startPaint = () => isPainting = true;
+
+const cancelPaint = () => {
+    isPainting = false;
+    ctx.beginPath();
 }
 
-function cancelPaint() {
-    isPainting = false;
-}
+const onLineWidthChange = (event) => ctx.lineWidth = event.target.value;    
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPaint);
 canvas.addEventListener("mouseup", cancelPaint);
 canvas.addEventListener("mouseleave", cancelPaint);
+
+lineWidth.addEventListener("change", onLineWidthChange);
